d32b0701fb1b4975bad969389c4f0ca13e037958
Vladimir Kavalerov <vladimir@kavalerov.net>
2023-02-19 21:52:50 +0100

    Initial commit

---
+++ weather-app/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Inter } from "@next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <div>Hello World</div>
+    </>
+  );
+}
