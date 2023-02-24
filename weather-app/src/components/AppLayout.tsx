036afcd89df23f3c542561e2a39bfb3da1fccc33
Daria Lazarenko <lazarenko200454@gmail.com>
2023-02-25 00:06:58 +0100

    Add simple AppShell in a new fold components

---
+++ weather-app/src/components/AppLayout.tsx
@@ -0,0 +1,29 @@
+import { AppShell, Navbar, Header, Text } from "@mantine/core";
+
+export default function AppLayout() {
+  return (
+    <AppShell
+      padding="md"
+      //   navbar={
+      //     <Navbar width={{ base: 300 }} p="xs">
+      //       <Text>Navbar text</Text>
+      //     </Navbar>
+      //   }
+      header={
+        <Header height={60} p="xs">
+          <Text>Header text</Text>
+        </Header>
+      }
+      styles={(theme) => ({
+        main: {
+          backgroundColor:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[8]
+              : theme.colors.gray[0],
+        },
+      })}
+    >
+      {/* Your application here */}
+    </AppShell>
+  );
+}
